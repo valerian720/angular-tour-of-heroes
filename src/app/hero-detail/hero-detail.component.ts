1212ebe4f455745e3ff7ffbd154f7fe23e7c0474
Валериан Воротников <valerian7202@gmail.com>
2023-06-20 20:07:50 +0300

    hero detail

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,11 @@
+import { Hero } from '../hero';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.scss']
+})
+export class HeroDetailComponent {
+@Input() hero?: Hero;
+}
