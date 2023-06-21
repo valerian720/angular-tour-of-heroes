c5c8fbc9b6cab8a82ce3b4a23c23e97de9f0f041
Валериан Воротников <valerian7202@gmail.com>
2023-06-21 11:41:16 +0300

    service hero + logging

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
+
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,14 +11,27 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent {
+  selectedHero?: Hero;
+  heroes: Hero[] = [];
+  //
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
+  //
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
-  heroes: Hero[] = HEROES;
-  selectedHero?: Hero;
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
+  
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-}
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
 }
