f13f2515165394b4bec1aa5a5f6dcbc2e26911b4
Валериан Воротников <valerian7202@gmail.com>
2023-06-21 12:26:07 +0300

    routing

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,10 @@
-import { Hero } from '../hero';
 import { Component, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService } from '../hero.service';
+
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-hero-detail',
@@ -7,5 +12,25 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./hero-detail.component.scss']
 })
 export class HeroDetailComponent {
-@Input() hero?: Hero;
+  constructor(
+      private route: ActivatedRoute,
+      private heroService: HeroService,
+      private location: Location
+    ) { }
+  // 
+  @Input() hero?: Hero;
+  // 
+  ngOnInit(): void {
+      this.getHero();
+  }
+  
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
